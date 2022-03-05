873e50ab17224c36c8b09712b679562911d4580f
Alex White <alexlloydwhite@gmail.com>
2022-03-05 09:29:19 -0600

    even more boilerplate

--- frontend/src/app/App.test.tsx
+++ frontend/src/app/App.test.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import { App } from "./App";
 
-test('renders learn react link', () => {
+test("renders text", () => {
+  // WHEN
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement: HTMLElement = screen.getByText(/Docker Frontend/i);
+
+  // THEN
   expect(linkElement).toBeInTheDocument();
 });
