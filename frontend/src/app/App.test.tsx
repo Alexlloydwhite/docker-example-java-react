66f6dcbd6ff8d92d72cfc46cac461ba4933a0aa2
Alex White <alexlloydwhite@gmail.com>
2022-03-05 22:31:02 -0600

    adding frontend to actions flow

--- frontend/src/app/App.test.tsx
+++ frontend/src/app/App.test.tsx
@@ -1,11 +1,22 @@
-import { render, screen } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 import { App } from "./App";
 
-test("renders text", () => {
-  // WHEN
-  render(<App />);
-  const linkElement: HTMLElement = screen.getByText(/Docker Frontend/i);
+describe("App", () => {
+  it("Renders Message", async () => {
+    // GIVEN:
+    const mockResponse = { dockerMessage: "Hello from Docker!" };
+    const mResponse = { json: jest.fn().mockResolvedValueOnce(mockResponse)};
+    const mockFetch = jest.fn().mockResolvedValueOnce(mResponse as any);
+    (global as any).fetch = mockFetch;
 
-  // THEN
-  expect(linkElement).toBeInTheDocument();
+    // WHEN:
+    const { getByTestId } = render(<App />);
+    const div = await waitFor(() => getByTestId('test'));
+
+    // THEN:
+    expect(div).toHaveTextContent('Hello from Docker!');
+    expect(mockFetch).toBeCalledTimes(1);
+    expect(mResponse.json).toBeCalledTimes(1);
+  });
 });
