00c0f95d8346b6ab1ea70de79c784ed6a182adb1
Alex White <alexlloydwhite@gmail.com>
2022-03-08 13:40:43 -0600

    adding produces annotation to return JSON

--- api/src/main/java/com/dockerapp/DockerMessageController.java
+++ api/src/main/java/com/dockerapp/DockerMessageController.java
@@ -1,8 +1,10 @@
 package com.dockerapp;
 
 import io.micronaut.http.HttpResponse;
+import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Produces;
 import jakarta.inject.Inject;
 
 @Controller("/")
@@ -12,6 +14,7 @@ public class DockerMessageController {
     DockerMessageService dockerMessageService;
 
     @Get("/getDockerMessage")
+    @Produces(MediaType.APPLICATION_JSON)
     HttpResponse<DockerMessage> getDockerMessage() {
         DockerMessage dockerMessage = dockerMessageService.getDockerMessage();
         return HttpResponse.ok(dockerMessage);
